52ef4afd93cdd635c683254d2a026266fc63e54a
Donald P Polansky <zizuar@comcast.net>
2018-12-16 23:57:30 -0600

    More coming in just a short to complete this,

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+// * There will be four crystals displayed as buttons on the page.
+// Mine will be rebel ships against the deathstar just to be different.
+
+// * The player will be shown a random number at the start of the game.
+// Score card layout will be on various areas of the page rather than a central location as in the past.
+
+// * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+// This should be educational and so I do not intend to show players the value after they click on one of the ships.
+// It will take just some simple math for them to figure out what the value of that ship is, and simple math to know what they need to win.
+// If I have to do math and logic for this darn thing again then there is no reason they can't either. ;-)
+
+
+
+//   * Your game will hide this amount until the player clicks a crystal.
+//   * When they do click one, update the player's score counter.
+
+// * The player wins if their total score matches the random number from the beginning of the game.
+
+// * The player loses if their score goes above the random number.
+
+// * The game restarts whenever the player wins or loses.
+
+//   * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. 
+//  Of course, the user's score (and score counter) will reset to zero.
+
+// * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
+// ##### Option 1 Game design notes
+
+// * The random number shown at the start of the game should be between 19 - 120.
+
+// * Each crystal should have a random hidden value between 1 - 12.
