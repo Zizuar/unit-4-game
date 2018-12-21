7c5cb22b2e4fc5b538995e430df16624260b4ebe
Donald P Polansky <zizuar@comcast.net>
2018-12-21 15:44:18 -0600

    Fixed tolkens, some syntax, and disabled Parallax.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,23 +7,19 @@ parallaxInstance.friction(0.9, 0.1);
 
 // * There will be four crystals displayed as buttons on the page.
 // Mine will be rebel ships against the deathstar just to be different.
-var ship1 = ;
-var ship2 = ;
-var ship3 = ;
-var ship4 = ;
-
+var ship1, ship2, ship3, ship4;
 
 // * The player will be shown a random number at the start of the game.
 // Score card layout will be on various areas of the page rather than a central location as in the past.
-var defense = ;
+var defense;
 // * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
 // This should be educational and so I do not intend to show players the value after they click on one of the ships.
 // It will take just some simple math for them to figure out what the value of that ship is, and simple math to know what they need to win.
 // If I have to do math and logic for this darn thing again then there is no reason they can't either. ;-)
 
-var pScore = ;
+var pScore;
 
-var scoreKeeper = ;
+var scoreKeeper;
 
 $("#ship1").on("click", function () {
     if (gameStarted === true) {
@@ -31,44 +27,44 @@ $("#ship1").on("click", function () {
         $("#pScore").text(scoreKeeper);
         results()
     }
-    else; {
+    else {
         return;
     }
-}
+})
 $("#ship2").on("click", function () {
     if (gameStarted === true) {
         scoreKeeper += ship2;
         $("#pScore").text(scoreKeeper);
         results()
     }
-    else; {
+    else {
         return;
     }
-}
+})
 $("#ship3").on("click", function () {
     if (gameStarted === true) {
         scoreKeeper += ship3;
         $("#pScore").text(scoreKeeper);
         results()
     }
-    else; {
+    else {
         return;
     }
-}
+})
 $("#ship4").on("click", function () {
     if (gameStarted === true) {
         scoreKeeper += ship4;
         $("#pScore").text(scoreKeeper);
         results()
     }
-    else; {
+    else {
         return;
     }
-}
+})
 //   * Your game will hide this amount until the player clicks a crystal.
 //   * When they do click one, update the player's score counter.
 
-var addScore = ;
+var addScore;
 
 
 // * The player wins if their total score matches the random number from the beginning of the game.
@@ -103,7 +99,7 @@ function results() {
 
 var gameStarted = false;
 
-function startNew().onclick {
+function startNew() {
     var gameStarted = true;
     random = Math.floor(Math.random() * 102 + 19);
     console.log(random);
@@ -117,5 +113,5 @@ function startNew().onclick {
 }
 
 // * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
-var wins = ;
-var loses = ;
+var wins;
+var loses;
